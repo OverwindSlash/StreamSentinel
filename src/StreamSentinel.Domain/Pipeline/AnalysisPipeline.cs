84cc63ecdc09d83a98d6424b4fbc559561e0e716
Overwind <overwind@outlook.com>
2024-03-28 18:34:22 +0800

    Add observer test cases

---
+++ src/StreamSentinel.Domain/Pipeline/AnalysisPipeline.cs
@@ -0,0 +1,12 @@
+﻿using StreamSentinel.Components.Interfaces.AnalysisEngine;
+using StreamSentinel.DataStructures;
+
+namespace StreamSentinel.Pipeline
+{
+    public class AnalysisPipeline
+    {
+        private const int DefaultFrameLifeTime = 125;
+        private readonly List<IAnalysisHandler> _analysisHandlers;
+        private readonly ObservableSlideWindow _slideWindow;
+    }
+}
